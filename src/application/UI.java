1ea0d0394bfd281bd71648c9729c79780cafc800
Pedro Antonio <pedro_arxp@hotmail.com>
2022-08-02 23:24:37 -0300

    Handiling exception and clearing screen/Tratando excecoes e limpando a tela

--- src/application/UI.java
+++ src/application/UI.java
@@ -30,6 +30,13 @@ public class UI {
 	public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
 	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 
+	// method to clear screen on command line
+	public static void clearScreen() {
+		System.out.print("\033[H\033[2J");
+		System.out.flush();
+	}
+
+	// method to read position on board  
 	public static ChessPosition readChessPosition(Scanner sc) {
 		try {
 			String s = sc.nextLine();
